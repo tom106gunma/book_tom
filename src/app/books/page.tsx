7355fce89e5a9446390adb114dd53e915f6390bd
tom106gunma <tom106.net@gmail.com>
2024-02-09 23:10:04 +0900

    googleapiで本の情報を取得

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,7 +1,25 @@
-export default function Page() {
+const getBooks = async () => {
+  const response = await fetch('https://www.googleapis.com/books/v1/volumes?q={search%20terms}');
+  const data = await response.json();
+  return data;
+};
+
+export default async function Page() {
+  const books = await getBooks();
+  console.log(books);
+  const book = books.items[0];
+
   return (
     <div>
       <h1>Books</h1>
+      <div className="flex flex-wrap gap-2">
+        {books.items.map((item: any) => (
+          <div key={item.id} className="border w-40 rounded">
+            <div>{item.volumeInfo.title}</div>
+            <img src={item.volumeInfo.imageLinks.thumbnail} alt="book image" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
