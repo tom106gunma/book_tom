736c494eefe1be819f9e0273df82725dab3e918d
tom106gunma <tom106.net@gmail.com>
2024-02-10 03:58:16 +0900

    loading画面を作成

---
+++ src/app/books/loading.tsx
@@ -0,0 +1,12 @@
+export default function loading() {
+  return (
+    <div className="flex gap-2">
+      <div className="w-48 h-72 bg-slate-600 animate-pulse rounded" />
+      <div className="w-48 h-72 bg-slate-600 animate-pulse rounded" />
+      <div className="w-48 h-72 bg-slate-600 animate-pulse rounded" />
+      <div className="w-48 h-72 bg-slate-600 animate-pulse rounded" />
+      <div className="w-48 h-72 bg-slate-600 animate-pulse rounded" />
+      <div className="w-48 h-72 bg-slate-600 animate-pulse rounded" />
+    </div>
+  );
+}
